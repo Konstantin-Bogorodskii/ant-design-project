be34910771c16df0148a62a51784182e24272928
Konstantin Bogorodskii <konstantin.bogorodskii@gmail.com>
2024-06-12 17:03:05 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from 'antd';
+
+export const App: React.FC = () => {
+	return (
+		<div className="App">
+			<Button type="primary">Button</Button>
+		</div>
+	);
+};
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@app': path.resolve(__dirname, './src/app'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@shared': path.resolve(__dirname, './src/shared'),
+			'@widgets': path.resolve(__dirname, './src/widgets'),
+			'@entities': path.resolve(__dirname, './src/entities'),
+			'@features': path.resolve(__dirname, './src/features')
+		}
+	},
+	server: {
+		port: 3000
+	}
+});
