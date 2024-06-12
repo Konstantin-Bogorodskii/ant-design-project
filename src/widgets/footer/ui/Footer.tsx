1041a19d14b77ebeaa876993f5ffdd9a75d900f7
Konstantin Bogorodskii <konstantin.bogorodskii@gmail.com>
2024-06-12 19:41:09 +0300

    create Header and Footer components added styling

--- src/widgets/footer/ui/Footer.tsx
+++ src/widgets/footer/ui/Footer.tsx
@@ -1,3 +1,19 @@
+import styles from './styles.module.css';
+import { Typography } from 'antd';
+
 export const Footer = () => {
-	return <footer>Footer</footer>;
+	return (
+		<footer className={styles.footer}>
+			<Typography.Link
+				href="https://policies.google.com/privacy"
+				target={'_blank'}>
+				Privacy Policy
+			</Typography.Link>
+			<Typography.Link
+				href="https://policies.google.com/terms"
+				target={'_blank'}>
+				Terms of Use
+			</Typography.Link>
+		</footer>
+	);
 };
--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,3 +1,31 @@
+import { Link } from 'react-router-dom';
+
+import { Typography, Space, Badge } from 'antd';
+import { MailOutlined, BellOutlined } from '@ant-design/icons';
+
+import { ROOT_PATH } from '@shared/constants';
+
+import styles from './styles.module.css';
+
 export const Header = () => {
-	return <header>Header</header>;
+	return (
+		<header className={styles.header}>
+			<Link to={ROOT_PATH}>
+				<Typography.Title style={{ marginBottom: 0, fontSize: 26 }}>
+					MyDashboard
+				</Typography.Title>
+			</Link>
+
+			<Space>
+				<Badge
+					count={10}
+					dot>
+					<MailOutlined className={styles.icon} />
+				</Badge>
+				<Badge count={2}>
+					<BellOutlined className={styles.icon} />
+				</Badge>
+			</Space>
+		</header>
+	);
 };
